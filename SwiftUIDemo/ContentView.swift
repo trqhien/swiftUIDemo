9f7f3d473959ba4283e55fe5e389cb173e9cfea5
Hien Quang Tran <hien.tran@wizeline.com>
2019-07-05 12:59:14 +0700

    Add Xcode project

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Hien Quang Tran on 7/5/19.
+//  Copyright © 2019 Hien Quang Tran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
